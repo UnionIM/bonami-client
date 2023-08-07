8845204d4dfee42778afb5852f12f7216296ca72
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-07 15:35:46 +0300

    Colors

---
+++ src/StyledComponents/colors.ts
@@ -0,0 +1,16 @@
+export const gray = {
+  lightest: "#EEEEEE",
+  light: "#EBEBEB",
+  default: "#B5B5B5",
+  dark: "#808080",
+  darkest: "#212121",
+};
+
+export const colorful = {
+  red: "#F63A3A",
+  lightRed: "#F95757",
+  yellow: "#F3F63A",
+  green: "#49F63A",
+  lightBlue: "#6A9DFF",
+  blue: "#1976D2",
+};
