5516e82385918ebe7d4c01cf394b261ef34e71bd
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-07 15:40:41 +0300

    Login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,40 +1,100 @@
-import React, { ChangeEvent, useState } from "react";
-import { Box, Button, Card, TextField, Typography } from "@mui/material";
-import PasswordInput from "../Components/UI/PasswordInput";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import {
+  Alert,
+  Button,
+  Card,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  PasswordInput,
+  Divider,
+  Link,
+  Alert as MyAlert,
+} from "../Components/UI/index";
 import { googleLogo } from "../Images/Vector";
-import Divider from "../Components/UI/Divider";
-import Link from "../Components/UI/Link";
+import BonamiController from "../Server/Controller/BonamiController";
+import { useDispatch } from "react-redux";
+import { IAlert } from "../Models/Bonami";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  const [inputError, setInputError] = useState<boolean>(false);
+
+  const [alert, setAlert] = useState<IAlert>({
+    isOpen: false,
+    message: "Error",
+    severity: "error",
+  });
+
+  const dispatch = useDispatch();
+
   const emailHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const signInHandler = () => {};
+  const signInHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    BonamiController.localLogin(email, password)
+      .then((res) => {
+        if (res?.status === 200) {
+          dispatch({ type: "SET_USER", payload: res.data });
+        }
+      })
+      .catch((e) => {
+        if (e.code === "ERR_NETWORK") {
+          setAlert({
+            isOpen: true,
+            message: "Internal Server Error",
+            severity: "error",
+          });
+        }
+        if (e.response?.status === 403) {
+          setInputError(true);
+        }
+      });
+  };
 
-  const googleSignInHandler = () => {};
+  const googleSignInHandler = () => {
+    window.open(`${process.env.REACT_APP_SERVER_URL}/google`, "_self");
+  };
 
   return (
-    <Box p={"60px"}>
+    <Grid p={"60px"} container justifyContent={"center"}>
       <Card sx={{ width: "415px", padding: "25px" }}>
         <Typography sx={{ mb: "25px" }}>Sign in</Typography>
-        <TextField
-          sx={{ width: "100%", mb: "20px" }}
-          placeholder={"Email"}
-          value={email}
-          onChange={emailHandler}
-        />
-        <PasswordInput value={password} setValue={setPassword} />
-        <Button
-          sx={{ width: "100%", m: "20px 0" }}
-          variant={"contained"}
-          onClick={signInHandler}
-        >
-          Sign in
-        </Button>
+        <form onSubmit={signInHandler}>
+          <TextField
+            sx={{ width: "100%", mb: "20px" }}
+            placeholder={"Email"}
+            value={email}
+            onChange={emailHandler}
+          />
+          <PasswordInput value={password} setValue={setPassword} />
+          <Button
+            sx={{ width: "100%", m: "20px 0" }}
+            variant={"contained"}
+            type={"submit"}
+          >
+            Sign in
+          </Button>
+        </form>
+        {inputError ? (
+          <Alert
+            severity="error"
+            onClose={() => {
+              setInputError(false);
+            }}
+          >
+            Wrong email or password
+          </Alert>
+        ) : (
+          <></>
+        )}
+
         <Divider text="or" />
         <Button
           sx={{ width: "100%", mt: "20px" }}
@@ -48,7 +108,8 @@ const Login = () => {
           Don’t have an account? <Link to={"sign-up"}>Sign up</Link>
         </Typography>
       </Card>
-    </Box>
+      <MyAlert state={alert} setState={setAlert}></MyAlert>
+    </Grid>
   );
 };
 
