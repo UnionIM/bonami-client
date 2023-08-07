a51ee5091fbf8662d5cf1509d60cd45c6f153420
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-07 15:40:22 +0300

    Exports

--- src/Components/UI/index.ts
+++ src/Components/UI/index.ts
@@ -1,3 +1,7 @@
 export { default as Searchbar } from "./Searchbar";
+export { default as PasswordInput } from "./PasswordInput";
+export { default as Divider } from "./Divider";
+export { default as Link } from "./Link";
 export { default as MySelect } from "./MySelect";
 export { default as LanguageSelectElement } from "./LanguageSelectElement";
+export { default as Alert } from "./Alert";
